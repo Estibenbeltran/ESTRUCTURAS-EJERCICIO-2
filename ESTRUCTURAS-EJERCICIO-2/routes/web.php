276e0c5417f34f187837a6d82b3d7a3e88640a5d
Dairo Estiben Beltrán <daiydan99@hotmail.com>
2018-03-15 18:08:35 -0500

    inclusion de routes web

--- ESTRUCTURAS-EJERCICIO-2/routes/web.php
+++ ESTRUCTURAS-EJERCICIO-2/routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('master');
 });
+Route::get('/resultados', function () {
+    return view('resultados');
+});
+Route::post('busqueda','Buscador@buscar');
